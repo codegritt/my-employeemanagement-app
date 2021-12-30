c2ca23f4541cea06d296e5041e219532812637af
Gokul <gokulchandan18@gmail.com>
2021-12-30 13:26:00 +0530

    commit

--- src/app/component/add-edit-user/add-edit-user.component.ts
+++ src/app/component/add-edit-user/add-edit-user.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder,FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-add-edit-user',
   templateUrl: './add-edit-user.component.html',
-  styleUrls: ['./add-edit-user.component.css']
+  styleUrls: ['./add-edit-user.component.scss']
 })
 export class AddEditUserComponent implements OnInit {
-
-  constructor() { }
+  addUserForm: FormGroup;
+  constructor(
+    public dialogRef: MatDialogRef<AddEditUserComponent>, 
+    public userService: UserService,
+    private formBuilder: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public incomingData: any
+  ) { }
 
   ngOnInit(): void {
+    this.addUserForm = new FormGroup({  
+        id: new FormControl(''),
+        firstName: new FormControl('', Validators.required),
+        lastName: new FormControl('', Validators.required),
+        email: new FormControl('', Validators.required),
+        contact: new FormControl('', Validators.required)
+    })
+    this.addUserForm.patchValue(this.incomingData);
   }
 
+  public closePopup() {
+    this.dialogRef.close();
+    this.addUserForm.reset();
+  }
+
+  public addUser() {
+    if (this.incomingData && this.incomingData.action && this.incomingData.action === 'edit') {
+      this.userService.editUser(this.incomingData.id, this.addUserForm.value);
+      this.dialogRef.close();
+    } else {
+      this.userService.addUser(this.addUserForm.value);
+      this.dialogRef.close();
+    }
+  }
+
+  public deleteUser() {
+    this.dialogRef.close(true);
+  }
+
+  public cancelDelete() {
+    this.dialogRef.close(false);
+  }
 }
