662e0a60c205b07118ec418418c144d5fa2ccbc1
Gokul <gokulchandan18@gmail.com>
2021-12-30 14:41:59 +0530

    commit

--- src/app/component/user/user.component.ts
+++ src/app/component/user/user.component.ts
@@ -12,7 +12,7 @@ import { UserService } from '../../services/user.service';
 })
 export class UserComponent implements OnInit {
   displayedColumns: string[] = ['Id','First Name', 'Last Name', 'Email', 'Contact', 'Action'];
-  userDetails!: MatTableDataSource<any>;
+  userDetails!: MatTableDataSource<Element>;
 
   constructor(
     public dialog: MatDialog,
