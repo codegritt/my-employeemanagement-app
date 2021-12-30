fc1a958b89ba907ff04d4ac01261c5d19df6e514
Gokul <gokulchandan18@gmail.com>
2021-12-30 13:12:24 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddEditUserComponent } from './component/add-edit-user/add-edit-user.component';
+import { UserComponent } from './component/user/user.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddEditUserComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
